71aa951a53a4680212d36ad7236db5c32d1a1ed3
Kyle Stewart <kylestew@gmail.com>
2020-05-03 09:22:31 -0700

    not sure I'm going in this direction

--- labs/01-miniblink/miniblink.c
+++ labs/01-miniblink/miniblink.c
@@ -1,15 +1,10 @@
-#include <libopencm3/stm32/gpio.h>
-#include <libopencm3/stm32/rcc.h>
 
 /*
 * Blinks onboard LED (PA8)
 */
 
+/*
 static void gpio_setup(void) {
-  /* NOTE: we are not setting up any clocks, this means the MCU will run
-   * on default settings using the internal oscillator.
-   */
-
   // Enable GPIOA clock
   // Manually:
   // RCC_AHB1ENR |= RCC_AHB1ENR_IOPAEN;
@@ -21,20 +16,13 @@ static void gpio_setup(void) {
   // GPIOA_CRH |= (GPIO_MODE_OUTPUT_2_MHZ << ((8 - 8) * 0));
   gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8);
 }
+*/
 
 int main(void) {
-  gpio_setup();
+  //gpio_setup();
 
-  // Blink the LED (PA8) on the board
-  int i;
-  int j = 0;
   while (1) {
-    // toggle LED on/off
-    gpio_toggle(GPIOA, GPIO8);
-    for (i = 0; i < 1000000; i++) {
-      __asm__("nop");
-    }
-    j++;
+    __asm__("nop");
   }
 
   return 0;
---
+++ labs/01-miniblink/miniblink.s
@@ -0,0 +1,36 @@
+	.cpu cortex-m4
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 1
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 1
+	.eabi_attribute 18, 4
+	.file	"miniblink.c"
+	.text
+	.align	1
+	.global	main
+	.arch armv7e-m
+	.syntax unified
+	.thumb
+	.thumb_func
+	.fpu softvfp
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	push	{r7}
+	add	r7, sp, #0
+.L2:
+	.syntax unified
+@ 25 "miniblink.c" 1
+	nop
+@ 0 "" 2
+	.thumb
+	.syntax unified
+	b	.L2
+	.size	main, .-main
+	.ident	"GCC: (GNU Tools for Arm Embedded Processors 8-2018-q4-major) 8.2.1 20181213 (release) [gcc-8-branch revision 267074]"
